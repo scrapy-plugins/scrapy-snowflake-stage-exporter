b5755ee4206fc8f74748a0fed08b22d69ecb6dd4
Oleg T <rapatka@yahoo.com>
2021-10-25 01:08:44 +0400

    bump version, update changelog

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="snowflake-stage-exporter",
-    version="0.0.3",
+    version="0.0.4",
     description="Snowflake database exporting utility with Scrapy integration",
     packages=find_packages(),
     python_requires=">=3.6",
