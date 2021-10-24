a451a96349a08a201f3753e9f838ce27eb7592e7
Oleg T <rapatka@yahoo.com>
2021-10-24 16:10:59 +0400

    fixes to normalizer

--- snowflake_stage_exporter/utils.py
+++ snowflake_stage_exporter/utils.py
@@ -17,9 +17,9 @@ def chunk(iterable: Iterable, n: int):
 def normalize_identifier(value: str) -> str:
     orig_value = value
     value = re.sub(r"(?i)[^a-z\d_$]+", " ", value).strip()
-    value = re.sub(r" +", "_", value)
-    if not re.match(r"(?i)[a-z]", value):
-        value = "_" + value
-    if value == "_":
+    if not value:
         return "_empty_" + sha256(orig_value.encode("utf8")).hexdigest()[:6]
+    value = re.sub(r" +", "_", value)
+    if not re.match(r"(?i)[_a-z]", value):
+        value = "_" + value
     return value
